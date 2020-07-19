d7feb396dcf0de16509222fa901b7afa72c3f457
Thang Nguyen <Thang.itus@gmail.com>
2020-07-20 00:16:12 +0700

    Init

--- ChatServer/src/main/java/ServerMain.java
+++ ChatServer/src/main/java/ServerMain.java
@@ -1,3 +1,28 @@
-package thangitus;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-class ServerMain {}
+public class ServerMain {
+   private static final int port = 8000;
+   private static final ExecutorService service = Executors.newCachedThreadPool();
+   public static void main(String[] args) {
+      try {
+         ServerSocket serverSocket = new ServerSocket(port);
+         System.out.println("Start server");
+         while (true) {
+            System.out.println("Waiting client....");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Accept client");
+            Worker worker = new Worker(clientSocket);
+            service.submit(worker);
+         }
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+
+   }
+
+}
--- ChatServer/src/main/java/Worker.java
+++ ChatServer/src/main/java/Worker.java
@@ -1,3 +1,33 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.net.Socket;
 
-class Worker {}
+public class Worker extends Thread {
+   private final Socket clientSocket;
+
+   public Worker(Socket clientSocket) {
+      this.clientSocket = clientSocket;
+   }
+   @Override
+   public void run() {
+      super.run();
+      handleClient();
+   }
+   private void handleClient() {
+      InputStream inputStream = null;
+      try {
+         inputStream = clientSocket.getInputStream();
+         OutputStream outputStream = clientSocket.getOutputStream();
+         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+         String line;
+         while ((line = bufferedReader.readLine()) != null) {
+            if (line.equalsIgnoreCase("quit"))
+               break;
+            String msg = "You: " + line + "\n";
+            outputStream.write(msg.getBytes());
+         }
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+
+   }
+}
